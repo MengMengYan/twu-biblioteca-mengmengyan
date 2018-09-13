e6dd8f1ed59d252f9dbd569a7dbb0d231863a741
mengmeng <mengmeng.yan@thoughtworks.com>
2018-09-13 17:07:43 +0200

    welcome message at entry point of app

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,8 +1,11 @@
 package com.twu.biblioteca;
 
+import com.twu.biblioteca.navigation.Menu;
+
 public class BibliotecaApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        Menu menu = new Menu();
+        System.out.println(menu.welcomeUser());
     }
 }
--- test/com/twu/biblioteca/BibliotecaAppTest.java
+++ test/com/twu/biblioteca/BibliotecaAppTest.java
@@ -1,10 +1,27 @@
 package com.twu.biblioteca;
 
 
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.junit.Assert.assertEquals;
 
 public class BibliotecaAppTest {
 
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @Test
+    public void testWelcomeMessage(){
+        BibliotecaApp.main(new String[]{});
+        assertEquals("Welcome to Biblioteca", outContent.toString().trim());
+    }
 
 }
